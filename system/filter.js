ccc77b8325fc41b8644e5214803b68202c8e1ca2
EaterUsr <129983819+EaterUsr@users.noreply.github.com>
2024-04-05 22:30:43 +0200

    feat: add filter

--- system/filter.js
+++ system/filter.js
@@ -1,15 +1,41 @@
-function filterLogos() {
-  var searchText = document.getElementById('search').value.toLowerCase();
+const logoContainer = document.querySelector("#logo-container");
+const searchInput = document.querySelector("#search");
 
-  var figures = document.querySelectorAll('figure');
+function filter(query, logos) {
+  if (query === "") {
+    logoContainer.innerHTML = logos
+      .map(logo => logoComponent(logo))
+      .join("");
+  }
 
-  figures.forEach(function(figure) {
-    var logoName = figure.dataset.name.toLowerCase();
-    if (logoName.includes(searchText)) {
-      figure.style.display = 'block'; 
-    } else {
-      figure.style.display = 'none'; 
-    }
+  const searchText = query.toLowerCase();
+
+  logoContainer.innerHTML = logos
+    .filter(logo => logo.name.includes(searchText))
+    .map(logo => logoComponent(logo))
+    .join("");
+}
+
+function logoComponent(logo) {
+  return `
+    <figure>
+      <img src="${logo.img}">
+      <figcaption>
+        <label class="top">${logo.name}</label>
+        <label class="right"><img src="./assets/contributors/${logo.contributor}.png" alt="${logo.contributor}">${logo.contributor}</label>
+        <label class="left">${logo.date}</label>
+      </figcaption>
+    </figure>
+  `;
+}
+
+async function getLogos() {
+  const response = await fetch("./logos.json");
+  const logos = await response.json()
+
+  searchInput.addEventListener("input", () => {
+    filter(searchInput.value, logos)
   });
 }
 
+getLogos();
